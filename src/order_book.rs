30daf40750408cb2804ae4722f24571d0f3babd5
Maximilian von Gaisberg <github@vongaisberg.de>
2019-12-25 00:19:28 +0100

    Add support for hot orders
    Does not fully work right now

--- src/order_book.rs
+++ src/order_book.rs
@@ -10,21 +10,22 @@ use bit_vec::BitVec;
 use std::collections::HashMap;
 
 use intmap::IntMap;
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::collections::btree_map::BTreeMap;
-use std::collections::btree_map::RangeMut;
-use std::ops::Range;
+//use std::collections::btree_map::RangeMut;
+//use std::ops::Range;
+use std::ops::Neg;
 
 use std::rc::Rc;
 use std::result::Result::*;
 
 const DEFAULT_HOT_PRICE_RANGE: Price = Price { val: 1024 };
-const DEFAULT_HOT_PRICE_BASE: Price = Price { val: 2048 };
+const DEFAULT_HOT_PRICE_BASE: Price = Price { val: 1 };
 const DEFAULT_HOT_MAP_CAPACITY: usize = 128;
 
 pub struct OrderBook {
-    hot_ask_set: BitVec,
-    hot_bid_set: BitVec,
+    hot_ask_set: RefCell<BitVec>,
+    hot_bid_set: RefCell<BitVec>,
 
     hot_price_base: Price,
     hot_price_range: Price,
@@ -47,8 +48,8 @@ pub struct OrderBook {
 impl Default for OrderBook {
     fn default() -> OrderBook {
         OrderBook {
-            hot_ask_set: BitVec::from_elem((DEFAULT_HOT_PRICE_RANGE).get() as usize, false),
-            hot_bid_set: BitVec::from_elem((DEFAULT_HOT_PRICE_RANGE).get() as usize, false),
+            hot_ask_set: RefCell::new(BitVec::from_elem((DEFAULT_HOT_PRICE_RANGE).get() as usize, false)),
+            hot_bid_set: RefCell::new(BitVec::from_elem((DEFAULT_HOT_PRICE_RANGE).get() as usize, false)),
 
             hot_ask_map: HashMap::with_capacity(DEFAULT_HOT_MAP_CAPACITY),
             hot_bid_map: HashMap::with_capacity(DEFAULT_HOT_MAP_CAPACITY),
@@ -79,22 +80,12 @@ impl OrderBook {
     ///
     ///
     fn match_order(&mut self, order: &mut Order) {
-        // First try to match the order with existing hot orders
-
-        let hot_set = match order.side {
-            //ASK orders match with bid orders
-            OrderSide::ASK => &self.hot_bid_set,
-
-            //ASK orders match with bid orders
-            OrderSide::BID => &self.hot_ask_set,
-        };
-
-        /// Match as much as possible from vol with bucket if price is right
-        /// Return the volume that is left over and the value that was payed for all matched volume
+        // Match as much as possible from vol with bucket if price is right
+        // Return the volume that is left over and the value that was payed for all matched volume
         let lambda = |(vol, val): (Volume, Value), (price, bucket): (&Price, &mut OrderBucket)| {
-            if order.matches_with(price) {
-                let vol_matched = bucket.match_orders(vol);
-                if vol_matched == vol {
+            if order.matches_with(&price) {
+                let vol_matched = bucket.match_orders(&vol);
+                if vol_matched == vol.clone() {
                     //Everything was matched, shortcut the try_fold
                     Err((Volume::ZERO, (val + (vol_matched * *price))))
                 } else {
@@ -106,20 +97,69 @@ impl OrderBook {
             }
         };
 
-        let (new_filled_vol, new_filled_val) = match match order.side {
-            // Using try_fold for the short_circuiting feature, when the order is already filled
-            // Err(x) and Ok(x) both mean that the order has been filled as much as possible and x volume remains in it
-            OrderSide::ASK => self
-                .cold_bid_map
-                .range_mut(..)
-                .try_rfold((order.volume, Value::ZERO), lambda),
-            OrderSide::BID => self
-                .cold_ask_map
-                .range_mut(..)
-                .try_fold((order.volume, Value::ZERO), lambda),
-        } {
-            Ok((vol, val)) => (order.volume - vol, val),
-            Err((vol, val)) => (order.volume - vol, val),
+        // First try to match the order with existing hot orders
+
+        let (hot_set, hot_map) = match order.side {
+            //ASK orders match with bid orders
+            OrderSide::ASK => (&self.hot_bid_set, &mut self.hot_bid_map),
+
+            //BID orders match with ask orders
+            OrderSide::BID => (&self.hot_ask_set, &mut self.hot_ask_map),
+        };
+
+        //How much volume is left in the order and how much was payed for the already matched volume
+        let (mut vol, mut val) = (order.volume, Value::ZERO);
+
+        //Base price and range of the hot set
+        let (hot_base, hot_range) = (self.hot_price_base.clone(), self.hot_price_range.clone());
+
+        let (new_filled_vol, new_filled_val) = match hot_set.borrow_mut()
+            .iter()
+            .enumerate()
+            .filter(|(_n, b)| *b)
+            .map(|(n, _b)| n.clone())
+            .try_for_each(|n| {
+                println!("Looking at hot_set entry {:?}", n);
+
+                println!("index={:?}", Self::hot_set_index_to_price_by_range(n, &order.side.clone().neg(), hot_base, hot_range));
+                let bucket = hot_map.get_mut(&Self::hot_set_index_to_price_by_range(n, &order.side.clone().neg(), hot_base, hot_range).get()).unwrap();
+
+                if order.matches_with(&bucket.price) {
+                    let vol_matched = bucket.match_orders(&vol);
+
+                    if bucket.total_volume.get() == 0 {
+                         hot_set.borrow_mut().set(n, false);
+                    }
+
+                    val += vol_matched * bucket.price;
+                    vol -= vol_matched;
+                    if vol_matched == vol.clone() {
+                        //Everything was matched, shortcut the try_for_each
+                        Err(())
+                    } else {
+                        //Not everything was matched, continue
+                        Ok(())
+                    }
+                } else {
+                    //Can't match any more, shortcircuit
+                    Err(())
+                }
+            }) {
+            Err(_) => (order.volume - vol, val),
+            Ok(_) => {
+                match match order.side {
+                    // Using try_fold for the short_circuiting feature, when the order is already filled
+                    // Err(x) and Ok(x) both mean that the order has been filled as much as possible and x volume remains in it
+                    OrderSide::ASK => self
+                        .cold_bid_map
+                        .range_mut(..)
+                        .try_rfold((vol, val), lambda),
+                    OrderSide::BID => self.cold_ask_map.range_mut(..).try_fold((vol, val), lambda),
+                } {
+                    Ok((vol, val)) => (order.volume - vol, val),
+                    Err((vol, val)) => (order.volume - vol, val),
+                }
+            }
         };
 
         order.filled_volume = Cell::new(new_filled_vol);
@@ -129,22 +169,36 @@ impl OrderBook {
         }
     }
 
-    fn hot_set_index_to_price(&self, index: usize, side: OrderSide) -> Price {
+    pub fn hot_set_index_to_price(&self, index: usize, side: &OrderSide) -> Price {
         match side {
             //For existing ask orders, we are interested in the lowest offer
+            //Index shifted up by hot_price_base
             OrderSide::ASK => Price::new(index as u64) + (self.hot_price_base),
             //For existing bid orders, we are interested in the highest offer
             OrderSide::BID => {
-                self.hot_price_range - (Price::new(index as u64) + (self.hot_price_base))
+
+                //(Upper limit minus index) shift up by hot_price_base
+                (self.hot_price_range - Price::new(index as u64)) + self.hot_price_base
             }
         }
     }
 
-    fn hot_set_price_to_index(&self, order: Order) -> usize {
+    pub fn hot_set_index_to_price_by_range(index: usize, side: &OrderSide, hot_price_base: Price, hot_price_range: Price) -> Price {
+        match side {
+            //For existing ask orders, we are interested in the lowest offer
+            OrderSide::ASK => Price::new(index as u64) + (hot_price_base),
+            //For existing bid orders, we are interested in the highest offer
+            OrderSide::BID => {
+                (hot_price_range - Price::new(index as u64)) + hot_price_base
+            }
+        }
+    }
+
+    pub fn hot_set_price_to_index(&self, order: &Order) -> usize {
         match order.side {
             OrderSide::ASK => (order.limit - self.hot_price_base).get() as usize,
             OrderSide::BID => {
-                (self.hot_price_base - (order.limit - self.hot_price_base)).get() as usize
+                (self.hot_price_range - (order.limit - self.hot_price_base)).get() as usize
             }
         }
     }
@@ -162,16 +216,15 @@ impl OrderBook {
     }
 
     fn get_or_create_hot_orderbucket(&mut self, order: &Order) -> &mut OrderBucket {
-        // Handling only for cold orders, hot orders are missing
         let order_map = match order.side {
             OrderSide::ASK => {
-                self.hot_ask_set
-                    .set((order.limit - self.hot_price_base).get() as usize, true);
+                self.hot_ask_set.borrow_mut()
+                    .set(self.hot_set_price_to_index(order), true);
                 &mut (self.hot_ask_map)
             }
             OrderSide::BID => {
-                self.hot_bid_set
-                    .set((order.limit - self.hot_price_base).get() as usize, true);
+                self.hot_bid_set.borrow_mut()
+                    .set(self.hot_set_price_to_index(order), true);
                 &mut (self.hot_bid_map)
             }
         };
@@ -182,7 +235,10 @@ impl OrderBook {
     }
 
     pub fn insert_order(&mut self, mut order: Order) {
+        println!("matching");
         self.match_order(&mut order);
+        println!("matched");
+
 
         if !order.is_filled() {
             if order.immediate_or_cancel {
@@ -191,18 +247,8 @@ impl OrderBook {
                 let order_rc = Rc::new(order);
 
                 if self.is_hot(order_rc.limit) {
-                    /*
-                    if order.side == OrderSide::ASK {
-                        self.hot_ask_set
-                            .set(self.hot_set_price_to_index(order.limit), true);
-                        let rc = Rc::new(order);
-                        self.hot_map.insert(self.hot_set_price_to_index(order.limit) as u64, rc);
-
-                    } else {
-                        self.hot_bid_set
-                            .set(self.hot_set_price_to_index(order.limit), true);
-                    }
-                    */
+                    self.get_or_create_hot_orderbucket(&order_rc)
+                        .insert_order(Rc::downgrade(&order_rc));
                 } else {
                     self.get_or_create_cold_orderbucket(&order_rc)
                         .insert_order(Rc::downgrade(&order_rc));
